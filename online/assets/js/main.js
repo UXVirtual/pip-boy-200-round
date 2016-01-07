e4dec33a5cb101821087652acec783285bae1660
Michael Andrew <michael@gladeye.co.nz>
2016-01-08 09:47:31 +1300

    No longer compiling bootstrap scss

--- online/assets/js/main.js
+++ online/assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
-    $.material.init()
+    console.log('document is ready')
+    $.material.init();
 });
 
--- online/public/config.js
+++ online/public/config.js
@@ -15,10 +15,7 @@ System.config({
   map: {
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
-    "bootstrap": "github:twbs/bootstrap@3.3.6",
-    "bootstrap-material-design": "npm:bootstrap-material-design@0.3.0",
     "core-js": "npm:core-js@1.2.6",
-    "jquery": "npm:jquery@1.11.3",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -31,23 +28,10 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
-    "github:twbs/bootstrap@3.3.6": {
-      "jquery": "github:components/jquery@2.1.4"
-    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.34": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:bootstrap-material-design@0.3.0": {
-      "bootstrap": "npm:bootstrap@3.3.6",
-      "jquery": "npm:jquery@1.11.3",
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:bootstrap@3.3.6": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@1.2.6": {
@@ -58,9 +42,6 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
-    },
-    "npm:jquery@1.11.3": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
