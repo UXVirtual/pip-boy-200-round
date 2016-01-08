dadc16d224a0e36c65c3667d3628d4ebbb74fade
Michael Andrew <michael@gladeye.co.nz>
2016-01-08 14:37:09 +1300

    Config page now loads correctly

---
+++ app/src/js/pebble-js-app.js
@@ -0,0 +1,28 @@
+Pebble.addEventListener('ready', function() {
+    console.log('PebbleKit JS ready!');
+});
+
+Pebble.addEventListener('showConfiguration', function() {
+    var url = 'http://michael.local:9000';
+
+    console.log('Showing configuration page: ' + url);
+
+    Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+    var configData = JSON.parse(decodeURIComponent(e.response));
+
+    console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+    if (configData.backgroundColor) {
+        Pebble.sendAppMessage({
+            backgroundColor: parseInt(configData.backgroundColor, 16),
+            twentyFourHourFormat: configData.twentyFourHourFormat
+        }, function() {
+            console.log('Send successful!');
+        }, function() {
+            console.log('Send failed!');
+        });
+    }
+});
